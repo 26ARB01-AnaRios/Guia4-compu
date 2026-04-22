969d2eb2727faa6b0052498728a93fc88ed87abb
26ARB01-AnaRios <riosana15db@gmail.com>
2026-04-22 13:54:15 -0600

    Add files via upload

---
+++ Guia4_IVBachillerato_anarios.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guia4_ivbachillerato_anarios;
+
+/**
+ *
+ * @author LABORATORIO
+ */
+public class Guia4_IVBachillerato_anarios {
+
+    public static void main(String[] args) {
+       jfrmMenu nuevo= new jfrmMenu();
+       nuevo.setVisible(true);
+    }
+}
